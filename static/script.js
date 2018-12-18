496b7546e7f8613aeb940e7eb4105e0c814e99cf
Lachlan <Lachlan@Lachlans-MacBook.local>
2018-12-18 20:28:58 +1100

    Static page script fixes

--- static/script.js
+++ static/script.js
@@ -1,30 +1,14 @@
 if (md_html) {
-    let index = -4
-    let more = true
 
-    while (more) {
-        let newindex = md_html.indexOf("<h", index + 4)
-        if (newindex == -1) {
-            more = false
-        } else {
-            index = newindex
-            let key = md_html.substring(index + 1, index + 3)
-            let contents = md_html.substring(md_html.indexOf("<" + key + ">") + 4, md_html.indexOf("</" + key + ">"))
-
-            let val;
-            let elms = document.querySelectorAll(key)
-            for (var i = 0; i < elms.length; i++) {
-                val = elms[i].innerHTML.toLowerCase().split(" ").join("-").split(".").join("")
-                elms[i].id = val
-            }
-
-            let elm = document.createElement("li")
-            elm.className = key
-            elm.innerHTML = contents
-            elm.setAttribute("onclick", "window.location.hash = '#" + val + "'")
-
-            document.querySelector(".contents-box").appendChild(elm)
-        }
+    let elms = document.querySelectorAll("h1").concat(document.querySelectorAll("h2"), document.querySelectorAll("h3"), document.querySelectorAll("h4"), document.querySelectorAll("h5"))
+    for (var i = 0; i < elms.length; i++) {
+        let val = elms[i].innerHTML.toLowerCase().split(" ").join("-").split(".").join("")
+        elms[i].id = val
+        let elm = document.createElement("li")
+        elm.className = elms[i].tagName.toLowerCase()
+        elm.innerHTML = elms[i].innerHTML
+        elm.setAttribute("onclick", "window.location.hash = '#" + val + "'")
+        document.querySelector(".contents-box").appendChild(elm)
     }
 
     document.querySelector(".content-container").innerHTML = md_html
